848c9c6861d20c3bdce5b6b97dddaf22ddacfdbc
Nicolas Codognotto <ncodognoto@gmail.com>
2024-08-20 01:25:00 -0300

    criando componentes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,27 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from "@/app/components/navbar/Navbar"
+import Footer from "@/app/components/footer/Footer"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+  title: 'Blogzinho',
+  description: 'Blogizinho teste',
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <div className="container">
+          <div className='wrapper'>
+            <Navbar/>
+            {children}
+            <Footer/>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "./components/featured/Featured";
+import CategoryList from "./components/categoryList/categoryList";
+import CardList from "./components/CardList/CardList";
+import Menu from "./components/Menu/Menu";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.container}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div> 
+  ); 
 }
